437a5addf2e71da62ee6cd96b5325c6d1e325c38
ysf3rdm <79582319+ysf3rdm@users.noreply.github.com>
2021-04-01 21:27:40 +0300

    commit

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -35,12 +35,21 @@ export class ProductAddComponent implements OnInit {
   add() {
     if (this.productAddForm.valid) {
       let productModel = Object.assign({}, this.productAddForm.value);
-      this.productService.add(productModel).subscribe((response) => {
-        console.log(response);
-        this.toastrService.success(response.message, 'Başarılı');
-      });
-    } else {
-      this.toastrService.error('Formunuz Hatalı', 'Dikkat');
+      this.productService.add(productModel).subscribe(
+        (response) => {
+          this.toastrService.success(response.message, 'Başarılı');
+        },
+        (responseError) => {
+          if (responseError.error.Errors.length > 0) {
+            for (let i = 0; i < responseError.error.Errors.length; i++) {
+              this.toastrService.error(
+                responseError.error.Errors[i].ErrorMessage,
+                'doğrulama hatası'
+              );
+            }
+          }
+        }
+      );
     }
   }
 }
